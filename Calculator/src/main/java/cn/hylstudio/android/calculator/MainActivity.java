5ecd48904d2c3bc1006232ee19cbe1a169caae1a
HYL <kuaile2911@126.com>
2016-09-02 08:41:47 +0800

    Initial commit

---
+++ Calculator/src/main/java/cn/hylstudio/android/calculator/MainActivity.java
@@ -0,0 +1,28 @@
+package cn.hylstudio.android.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by HYL on 2016/8/30.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    /**
+     * 初始化所有控件
+     */
+    private void init() {
+
+    }
+}
